b456dcec305d1ac272e4b6c32c415ecb53b88e03
sandeepMona <sandeepMona@192.168.1.12>
2019-12-16 00:07:18 +0530

    tree

---
+++ Selenium_Project1/src/JAVA_Programes/TreeSetCls.java
@@ -0,0 +1,17 @@
+package JAVA_Programes;
+
+import java.util.TreeSet;
+
+public class TreeSetCls {
+	public static void main(String[] args)
+	{
+		TreeSet a=new TreeSet();
+		a.add(2);
+		a.add(3);
+		a.add(5);
+		a.add(0);
+		a.add(9);
+		System.out.println(a);
+		
+	}
+}
